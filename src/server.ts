cea871719a48b777fe551268a0e58900127034b0
zhshuvo <zhshuvo99@gmail.com>
2023-06-04 21:17:51 +0600

    feat: implemented logger

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
 import mongoose from "mongoose";
 import app from "./app";
 import config from "./config";
+import { errorLogger, infoLogger } from "./general/logger";
 
 async function main() {
     try {
         await mongoose.connect(config.database_url as string);
         app.listen(config.port, () => {
-            console.log(
-                "\x1b[32m%s\x1b[0m",
-                `Server is listening on port ${config.port}`
+            infoLogger.info(
+                `---->Server is listening on port ${config.port}<----`
             );
         });
-        console.log("\x1b[32m%s\x1b[0m", `Database connected`);
+        infoLogger.info("---->Database connected<----");
     } catch (error) {
-        console.log("\x1b[31m%s\x1b[0m", `Database connection error, ${error}`);
+        errorLogger.error(`---->Database connection error, ${error}<----`);
     }
 }
 
