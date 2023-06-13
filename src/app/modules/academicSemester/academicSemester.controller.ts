c69e6c7363d45ceb246b1ad747a3c7b84a5d4b98
zhshuvo <zhshuvo99@gmail.com>
2023-06-13 12:48:15 +0600

    refactor: Modify and update code

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,22 +1,24 @@
-import { RequestHandler } from "express";
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+import catchAsync from "../../../general/catchAsync";
+import sendResponse from "../../../general/sendResponse";
 import { AcademicSemesterService } from "./academicSemester.service";
 
-const createSemester: RequestHandler = async (req, res, next) => {
-    try {
+const createSemester = catchAsync(
+    async (req: Request, res: Response, next: NextFunction) => {
         const { ...academicSemesterData } = req.body;
-        const newUser = await AcademicSemesterService.createSemester(
+        const newSemester = await AcademicSemesterService.createSemester(
             academicSemesterData
         );
-        res.status(200).json({
+        next();
+        sendResponse(res, {
+            statusCode: httpStatus.OK,
             operation: "Successful",
             message: "Semester created successfully",
-            data: newUser,
+            data: newSemester,
         });
-        return next();
-    } catch (error) {
-        next(error);
     }
-};
+);
 
 export const AcademicSemesterController = {
     createSemester,
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,20 +1,27 @@
-import { RequestHandler } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
+import httpStatus from "http-status";
+import catchAsync from "../../../general/catchAsync";
+import sendResponse from "../../../general/sendResponse";
 import { UserSevice } from "./user.service";
 
-const createdUser: RequestHandler = async (req, res, next) => {
-    try {
+const createdUser: RequestHandler = catchAsync(
+    async (req: Request, res: Response, next: NextFunction) => {
         const { user } = req.body;
         const newUser = await UserSevice.createUser(user);
-        res.status(200).json({
+        next();
+        // res.status(200).json({
+        //     operation: "Successful",
+        //     message: "User created successfully",
+        //     data: newUser,
+        // });
+        sendResponse(res, {
+            statusCode: httpStatus.OK,
             operation: "Successful",
             message: "User created successfully",
             data: newUser,
         });
-        return next();
-    } catch (error) {
-        next(error);
     }
-};
+);
 
 export const UserController = {
     createdUser,
